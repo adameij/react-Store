eef69aef88978c38efc338df528067ef3d0885a6
adameij <220532@supinfo.com>
2020-02-28 10:00:50 +0100

    2 files modified, 1 added

---
+++ components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Product extends React.Component{
+  render(){
+    return(
+      <li> Shoe</li>
+        
+    )
+  }
+}
+
+export default Product
--- components/ProductList.js
+++ components/ProductList.js
@@ -1,10 +1,22 @@
 import React,{Component} from 'react'
-
+import Product from './Product'
 class ProductList extends Component{
   render(){
     return(
       <div>
-        hello world!
+        <div className="card">
+                    <div className="card-header">
+                        <h1 className="m-3">Products list</h1>
+
+                    </div>
+                    <div className="card-body">
+                        <ul className="list-group m-3">
+                            {
+                                <Product/>
+                            }
+                        </ul>
+                    </div>
+                </div>
       </div>
     )
   }
